d49e61a0fe311ba2dc377954dfed096bb7b336b8
Atsushi <todaatsushi0@gmail.com>
2019-05-01 17:28:45 +0100

    Cleaned settings and urls of code bloat

--- champ/settings.py
+++ champ/settings.py
@@ -4,16 +4,8 @@ import os
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv('DEBUG') == 'True')
-
 ALLOWED_HOSTS = ['localhost']
 
 
